efe16fbd2b0cf14c0fe251e0107ba3e7ea26c42d
Luisa Coelho <luisa@omnicode.solutions>
2024-05-22 09:22:48 -0300

    feat: write translation in .pt.md file

--- translate.py
+++ translate.py
@@ -5,12 +5,14 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 if len(sys.argv) > 1:
-  path = f'content/posts/{sys.argv[1]}.md'
+  en_path = f'content/posts/{sys.argv[1]}.md'
+  pt_path = f'content/posts/{sys.argv[1]}.pt.md'
 else:
   date = datetime.today().strftime('%Y-%m-%d')
-  path = f'content/posts/{date}.md'
+  en_path = f'content/posts/{date}.md'
+  pt_path = f'content/posts/{date}.pt.md'
 
-text = open(path, 'r+')
+en_file = open(en_path, 'r+')
 
 load_dotenv()
 client = OpenAI()
@@ -24,7 +26,7 @@ response = client.chat.completions.create(
     },
     {
       "role": "user",
-      "content": text.read()
+      "content": en_file.read()
     }
   ],
   temperature=0.7,
@@ -32,6 +34,10 @@ response = client.chat.completions.create(
   top_p=1
 )
 
-text.close()
+translated_text = response.choices[0].message.content
 
-print(response.choices[0].message.content)
+pt_file = open(pt_path, 'w')
+pt_file.write(translated_text)
+pt_file.close()
+
+en_file.close()
