48479eeea7b571beda70c1c5a0a37fe9bd21b6a8
Luisa Coelho <luisa@omnicode.solutions>
2024-05-28 11:39:02 -0300

    chore: organize code

--- ingest_data.py
+++ ingest_data.py
@@ -5,32 +5,29 @@ import chromadb
 
 dotenv.load_dotenv()
 
-client = chromadb.Client()
-collection = client.create_collection("en_posts")
-documents = []
-metadatas = []
-ids = []
+def create_collection(folder_path, en_files):
+    client = chromadb.Client()
+    collection = client.create_collection("en_posts")
+    documents = []
+    metadatas = []
+    ids = []
 
-folder_path = "content/posts/"
-files = os.listdir(folder_path)
-en_files = [file for file in files if ('.pt' not in file) and ('_index' not in file) and ('2023' or '2024-01' or '2024-02' or '2024-03-01' in file)]
+    for file in en_files:
+        markdown_path = f"{folder_path}{file}"
+        loader = UnstructuredMarkdownLoader(markdown_path)
 
-for file in en_files:
-    markdown_path = f"{folder_path}{file}"
-    loader = UnstructuredMarkdownLoader(markdown_path)
+        raw_documents = loader.load()
+        content = raw_documents[0].page_content
 
-    raw_documents = loader.load()
-    content = raw_documents[0].page_content
+        documents.append(content)
+        metadatas.append({"source": folder_path + file})
+        ids.append(file)
 
-    documents.append(content)
-    metadatas.append({"source": folder_path + file})
-    ids.append(file)
+    collection.add(
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
+    )
 
-collection.add(
-    documents=documents,
-    metadatas=metadatas,
-    ids=ids,
-)
-
-with open("vectorstore.pkl", "w") as f:
-	f.write(str(collection.get()))
+    with open("blog_posts.pkl", "w") as f:
+        f.write(str(collection.get()))
--- query_data.py
+++ query_data.py
@@ -2,26 +2,44 @@ from langchain.chains.llm import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.prompts.prompt import PromptTemplate
-from langchain.memory import ConversationBufferMemory
 import dotenv
 
 dotenv.load_dotenv()
 
-_template = """Given the following blog posts about what was done each day in an internship program, create a general internship report with three main sections: activities executed, methodologies applied and conclusion. No need for timestamps.
+def create_report_from_blog():
+    _template = """Given the following blog posts about what was done each day in an internship program, create a general internship report with three main sections: activities executed, methodologies applied and conclusion. No need for timestamps.
 
-Blog posts:
-{blog_posts}
-Internship report:"""
-prompt = PromptTemplate.from_template(_template)
+    Blog posts:
+    {blog_posts}
+    Internship report:"""
+    prompt = PromptTemplate.from_template(_template)
 
-with open("vectorstore.pkl", "r") as f:
-    vectorstore = f.read()
+    with open("blog_posts.pkl", "r") as f:
+        blog_posts = f.read()
 
-llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
-memory = ConversationBufferMemory(
-    memory_key="chat_history", return_messages=True)
-llm_chain = LLMChain(llm=llm, prompt=prompt)
+    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
 
-response = llm_chain.run(vectorstore)
-with open("report.md", "w") as r:
-    r.write(response)
+    response = llm_chain.run(blog_posts)
+    return response
+
+def create_report_from_reports():
+    _template = """Given the following reports from an internship program, create a general report with three main sections: activities executed, methodologies applied and conclusion.
+
+    Reports:
+    {reports}
+    General report:"""
+    prompt = PromptTemplate.from_template(_template)
+
+    with open("reports.pkl", "r") as f:
+        vectorstore = f.read()
+
+    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = llm_chain.run(vectorstore)
+    return response
+
+def save_report(response):
+    with open("report.md", "w") as r:
+        r.write(response)
